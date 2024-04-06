1705ad1364fa0c842d426136c109519ca7d7d30f
quinny26 <20026235@tafe.wa.edu.au>
2024-04-07 02:05:53 +0800

    Refactor routers

--- public/index.php
+++ public/index.php
@@ -2,4 +2,14 @@
 
 require '../helpers.php';
 
-require basePath('views/home.view.php');
+require basePath('Router.php');
+
+$router = new Router ();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+
