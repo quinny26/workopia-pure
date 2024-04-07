58306ccac7eaaa28522dadd71dc03f1adcb0d9fa
quinny26 <20026235@tafe.wa.edu.au>
2024-04-08 03:44:20 +0800

    Add new path to Database.php and parse url

--- public/index.php
+++ public/index.php
@@ -1,15 +1,19 @@
 <?php 
 
 require '../helpers.php';
-
 require basePath('Router.php');
+require basePath('Database.php');
 
+//Instantiate the router
 $router = new Router ();
 
+//get routes
 $routes = require basePath('routes.php');
 
-$uri = $_SERVER['REQUEST_URI'];
+//get the current uri and http method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
+//route the request
 $router->route($uri, $method);
 
