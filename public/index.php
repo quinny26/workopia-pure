ea615e46677566c5b007e1da7d2b6b66d564fa94
quinny26 <20026235@tafe.wa.edu.au>
2024-04-08 16:06:41 +0800

    Create and use autoloader

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php 
-
+require __DIR__.'/../vendor/autoload.php';
 require '../helpers.php';
-require basePath('Router.php');
-require basePath('Database.php');
+
+use Framework\Router;
+
+// spl_autoload_register(function($class){
+//     $path = basePath('Framework/'.$class.'.php');
+//     if (file_exists($path)){
+//         require $path;
+//     }
+// });
 
 //Instantiate the router
 $router = new Router ();
@@ -12,8 +19,8 @@ $routes = require basePath('routes.php');
 
 //get the current uri and http method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+
 
 //route the request
-$router->route($uri, $method);
+$router->route($uri);
 
