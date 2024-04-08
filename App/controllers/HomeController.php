99a23a81a1fad664b015142ae7f5a90a32c7c30a
quinny26 <20026235@tafe.wa.edu.au>
2024-04-08 16:00:46 +0800

    Create home controller class

---
+++ App/controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+
+class HomeController {
+
+    protected $db;
+
+    /**
+     * show the lastest listing
+     * 
+     * @return void
+     */
+
+    public function __construct() {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function index(){
+        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+        loadView('home', [
+            'listings'=> $listings
+        ]);
+    }
+}
