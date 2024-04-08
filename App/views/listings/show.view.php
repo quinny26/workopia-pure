28be266638d86d1156438810cf8a3a7e56a863fa
quinny26 <20026235@tafe.wa.edu.au>
2024-04-09 02:09:29 +0800

    Add the flash messages Add function to check if tags are empty Change the URL path

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -4,15 +4,17 @@
 
 <section class="container mx-auto p-4 mt-4">
       <div class="rounded-lg shadow-md bg-white p-3">
+      <?= loadPartial('message') ?>
        <div class="flex justify-between items-center">
       <a class="block p-4 text-blue-700" href="/listings">
         <i class="fa fa-arrow-alt-circle-left"></i>
         Back To Listings
       </a>
       <div class="flex space-x-4 ml-4">
-        <a href="/edit" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+        <a href="/listings/edit/<?= $listing->id ?>" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
         <!-- Delete Form -->
         <form method="POST">
+          <input type = "hidden" name = "_method" value = "DELETE">
           <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded">Delete</button>
         </form>
         <!-- End Delete Form -->
@@ -27,9 +29,11 @@
               <strong>Location:</strong> <?= $listing->city ?>, <?= $listing->state ?>
               <!-- <span class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span> -->
             </li>
-            <li class="mb-2">
-              <strong>Tags:</strong> <?= $listing->tags ?>
-            </li>
+            <?php if (!empty($listing->tags)) : ?>
+                <li class="mb-2">
+                  <strong>Tags:</strong> <?= $listing-> tags ?>
+                </li>
+            <?php endif ; ?>
           </ul>
         </div>
       </div>
