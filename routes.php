cd32a33a6b90971a7fe2a1f7e5f952aa3ff3c70f
quinny26 <20026235@tafe.wa.edu.au>
2024-04-10 03:10:31 +0800

    Add routes to UserController

--- routes.php
+++ routes.php
@@ -9,3 +9,8 @@ $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 
 $router->delete('/listings/{id}', 'ListingController@destroy');
+
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
+
+$router->post('/auth/register', 'UserController@store');
