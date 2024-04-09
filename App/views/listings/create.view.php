1b0aa609424a610f5e6a2fd1f631a8f8b215f664
quinny26 <20026235@tafe.wa.edu.au>
2024-04-10 03:13:17 +0800

    Move error messages to a partial and load the partial in create and edit view files

--- App/views/listings/create.view.php
+++ App/views/listings/create.view.php
@@ -8,11 +8,9 @@
         <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
         <form method="POST" action="/listings">
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">Job Info</h2>
-          <?php if(isset($errors)) :?>
-            <?php foreach($errors as $error) :?>
-              <div class="message bg-red-100 my-3"><?= $error ?></div>
-            <?php endforeach; ?>
-            <?php endif; ?>
+          <?= loadPartial('errors', [
+            'errors'=> $errors ?? []
+          ]) ?>
           <div class="mb-4"><input type="text" name="title" placeholder="Job Title" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?=$listing['title'] ?? '' ?>"></div>
 
           <div class="mb-4"><textarea name="description" placeholder="Job Description" class="w-full px-4 py-2 border rounded focus:outline-none"><?=$listing['description'] ?? '' ?></textarea></div>
--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -9,11 +9,9 @@
         <form method="POST" action="/listings/<?= $listing->id ?>">
         <input type="hidden" name="_method" value= "PUT">
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">Job Info</h2>
-          <?php if(isset($errors)) :?>
-            <?php foreach($errors as $error) :?>
-              <div class="message bg-red-100 my-3"><?= $error ?></div>
-            <?php endforeach; ?>
-            <?php endif; ?>
+          <?= loadPartial('errors', [
+            'errors'=> $errors ?? []
+          ]) ?>
           <div class="mb-4"><input type="text" name="title" placeholder="Job Title" class="w-full px-4 py-2 border rounded focus:outline-none" value="<?=$listing->title ?? '' ?>"></div>
 
           <div class="mb-4"><textarea name="description" placeholder="Job Description" class="w-full px-4 py-2 border rounded focus:outline-none"><?=$listing->description ?? '' ?></textarea></div>
